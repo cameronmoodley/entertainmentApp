602ed8a2d6044100732c8267e08ce700c9a075cc
Merethe <merethe.myrhaug@gmail.com>
2019-10-09 15:00:37 +0200

    saving changes before jon smashes the code

--- js/music.js
+++ js/music.js
@@ -1,50 +1,102 @@
 const searchBtn = document.querySelector('#searchButton');
-let searchFieldValue;
+let albumContainer = document.querySelector('.album-container');
+let searchtext = document.querySelector("#search").value;
+let top10 = document.querySelector('.toplist__content');
+
+searchBtn.addEventListener('click',() => {
+    searchResult();
+    topTenResults();
+});
 
 
-searchBtn.addEventListener('click', (event) => {
-    event.preventDefault();
-    container.innerHTML = '<div class="loading"><img src="images/loading.gif"></div>';
-    lookUp();
-  });
-
-
-
-
-function getSearchText() {
-    let search = document.querySelector("#searchInput").value;
-    if(search.value = null){
-        document.querySelector('.h3').innerHTML = "We cound not find the artist you were searching for!";
-    }else{
-        document.querySelector('.h3').innerHTML = `Music by ${srtArtist}`;
-
-    }
-
-}
-
-
-function searchResult(searchtext) {
+  function searchResult() {
     fetch(`https://theaudiodb.com/api/v1/json/195003/searchalbum.php?s=${searchtext}`)
         .then((response) => response.json())
-        .then((json) => albumCard(json.track))
+        .then((json) => {
+            console.log(json)
+            albumContainer.innerHTML = "";
+            top10.innerHTML = ""
+            albumCard(json.album)
+        })
 }
-searchResult();
-
-
-let card = document.querySelector('body');
 
 function albumCard (array) {
+    console.log(array);
     array.map( (object) => {
 
-        card.innerHTML += `
-        <div class="[ card ]">
-        <img src="${object.strMusicVidScreen1}">
-        <h3 class="[ albumTitel ]" id="${object.strAlbum}"></h3> 
-        <p class="[ albumDate ]" id="${object.strGenre}"></p>
-        </div>
-        
-        `
+let albumCover = "";
+if(object.strAlbumThumb){
+            albumCover = object.strAlbumThumb;
+        }else {
+            albumCover = src="img/defult-album-notes.jpg"
+        }
 
+        
+        albumContainer.innerHTML += `
+        <div class="album-container" id="albumContainer">
+                <div class="album">
+                    <div class="album__image">
+                        <img src="${albumCover}" alt="${object.strAlbum}">
+                    </div>
+                    <div class="album__title">
+                    <h3>${object.strAlbum}</h3>
+                    </div>
+                    <div class="album__year">
+                    <p>${object.strGenre}</p>
+                    </div>
+                </div>
+        `
     })
     
     }
+
+    function topTenResults() {
+        fetch(`https://theaudiodb.com/api/v1/json/195003/track-top10.php?s=simplyred&s=${searchtext}`)
+        .then((response) => response.json())
+        .then((json) => {
+            console.log(json)
+            topTenList(json.track)
+        })
+    }
+    topTenResults();
+    
+    function topTenList(array) {
+        console.log(array);
+    array.map( (object) => {
+        top10.innerHTML += `
+                <ul>
+                    <li>${object.strTrack}</li>
+                </ul>
+        `
+    })
+    }
+
+        /*if(search.value = null){
+            document.querySelector('.album__title').innerHTML = "We cound not find the artist you were searching for!";
+        }else{
+            document.querySelector('.album__title').innerHTML = `Music by ${object.srtArtist}`;
+    
+        }*/
+
+/* 
+
+      <div class="[ card ]">
+        <img src=">
+        <h3 class="[ albumTitel ]" id=""></h3> 
+        <p class="[ albumDate ]" id="${object.strGenre}"></p>
+        </div>
+
+
+
+*/
+
+
+
+/*function searchResult(searchtext) {
+    fetch(`https://theaudiodb.com/api/v1/json/195003/searchalbum.php?s=${searchtext}`)
+        .then((response) => response.json())
+        .then((json) => albumCard(json.track))
+}*/
+
+
+
