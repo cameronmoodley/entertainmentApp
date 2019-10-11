5bb260e1e050ec53d7a3f6c13554adfe267018fa
Remano <remanovictor@icloud.coms>
2019-10-11 22:42:11 +0200

    fixed search button

--- js/movies.js
+++ js/movies.js
@@ -21,19 +21,20 @@ function runSearch() {
                 let data = result;
                 if (!data.Error) {
                     console.log(data)
-                    document.getElementById('movies').innerHTML += `<div>
-                <h1>${data.Title}<h1> 
-                <img src="${data.Poster}">
-                <div><span><p>${(data.Ratings.length > 1) ? data.Ratings[1].Source : 'No Rating Available'}</p></span><span><p>${(data.Ratings.length > 1) ? data.Ratings[1].Value : " "}</p></span></div>
-                <p> ${data.Genre}</p>
-                <p> ${data.Plot}</p>
-                </div>`
+                    document.getElementById('movies').innerHTML += `
+                    <div>
+                        <h1>${data.Title}<h1> 
+                        <img src="${data.Poster}">                        
+                        <div><span><p>${(data.Ratings.length > 1) ? data.Ratings[1].Source : 'No Rating Available'}</p></span><span><p>${(data.Ratings.length > 1) ? data.Ratings[1].Value : " "}</p></span></div>                        
+                        <p> ${data.Genre}</p>
+                        <p> ${data.Plot}</p>                        
+                    </div>`
                 } else {
                     console.log("The movie you are looking for can not be found.")
                 }
             })
     } else {
-        console.log("The movie you are looking for can not be found.")
+        console.log("The movie you are looking for can not be found.");
     }
 }
 
@@ -41,6 +42,7 @@ function runSearch() {
 // loading GIF
 
 function loading() {
+    document.innerHTML="";
     let movies = document.getElementById('movies');
     movies.innerHTML = "";
     movies.innerHTML += `<div class="[ loading-animation ]"></div>`;
@@ -70,7 +72,7 @@ function storeSearch() {
 
 //slider code
 
-$(document).ready(function(){
+$(document).ready(function () {
     $('.movie-poster').slick({
         slidesToShow: 6,
         slidesToScroll: 1,
